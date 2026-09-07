5f4b21d295aa8581af428280b5cab02f3b6fe716
stdrouuu <brajs0111@gmail.com>
2026-09-07 11:19:49 +0700

    +1 build version for .aab

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "org.ukrida.labvora"
         minSdk = 27
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
